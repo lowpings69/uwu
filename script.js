045bde3ac63bc2b449c87ce073b559c0777392dd
lowpings69 <egypttiktok587@gmail.com>
2025-03-25 15:31:38 +0300

    bgVideo

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ bgVideo.src = getVideo;
 credit_text.innerHTML = getVideo === 'source/videos/0.mp3'
     ? `🎵 <a href="https://www.youtube.com/watch?v=X4DORgzndYI" target="_blank">Vibe Tracks - I Love You</a><br><br>Website by <a href="https://t.me/lowpings" target="_blank">@LowPings</a>`
     : `🎬 ${videoName}<br><br>Thanks <a href="https://www.tiktok.com/@lyoshnka" target="_blank">@lyoshnka</a> for videos.<br>Website by <a href="https://t.me/lowpings" target="_blank">@LowPings</a>`;
-if (getVideo === 'source/videos/0.mp3') bgVideo.style.animation = 'spin 15s linear infinite';
+if (getVideo === 'source/videos/0.mp3') bgVideo.style.animation = 'spin 15s linear infinite' else bgVideo.style.display = 'none';
 function resoulatorFunc() { resoulatorElement.innerHTML = `📺 Display size: <span style="color: #aaffff">${window.innerWidth}x${window.innerHeight}</span>`; }  
 resoulatorFunc();
 window.addEventListener('resize', resoulatorFunc);
